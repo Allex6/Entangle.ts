1d557a272b25929005e50699ea4239b0e1ed83c5
Alex Souza <alex.webdevelloper@gmail.com>
2025-06-07 17:08:21 -0300

    :tada: feat: Implement EventHorizon class for event history management and querying

---
+++ src/EventHorizon.ts
@@ -0,0 +1,30 @@
+import { QueryBuilder } from './builders/Query.builder';
+import { CausalityLog } from './types/Events.types';
+
+/**
+ * The limit from which data cannot escape. It holds a history of all events ocurred and allows the search of it
+ */
+export class EventHorizon {
+  public readonly causalityLogs: CausalityLog[] = [];
+
+  /**
+   * Add data to the Event Horizon so it can be queried later
+   * @param event
+   * @param args
+   * @returns
+   */
+  public add<EventArgs extends unknown[] = unknown[]>(
+    event: string,
+    ...args: EventArgs
+  ): this {
+    this.causalityLogs.push({ event, args });
+    return this;
+  }
+
+  /**
+   * It allows the query of events that has already happened
+   */
+  public query(): QueryBuilder {
+    return new QueryBuilder(this.causalityLogs);
+  }
+}
