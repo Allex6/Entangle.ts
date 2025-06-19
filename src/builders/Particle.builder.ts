a6bab71b5e8e4801f03599e14f88e57a3dbab859
Alex Souza <alex.webdevelloper@gmail.com>
2025-06-19 19:17:07 -0300

    :sparkles: feat: Enhance ParticleContractBuilder with event and condition handling

--- src/builders/Particle.builder.ts
+++ src/builders/Particle.builder.ts
@@ -1,3 +1,4 @@
+import { HiggsField } from '../HiggsField';
 import { Superposition } from '../Superposition';
 import { IParticleCreation } from '../types/Particles.types';
 import { Particle } from '../types/Utils.types';
@@ -5,11 +6,15 @@ import { Particle } from '../types/Utils.types';
 export class ParticleContractBuilder {
   private readonly contract: Partial<IParticleCreation> = {};
 
-  constructor(private readonly parent: Superposition) {}
-
-  public upon(event: string): this {
-    this.contract.upon = event;
-    return this;
+  constructor(
+    private readonly parent: Superposition,
+    private readonly _event: string,
+    private readonly _when?: string,
+    private readonly _is?: any
+  ) {
+    this.contract.upon = this._event;
+    this.contract.when = this._when;
+    this.contract.is = this._is;
   }
 
   public build<ParticleType>(particleClass: Particle<ParticleType>): this {
@@ -17,31 +22,21 @@ export class ParticleContractBuilder {
     return this;
   }
 
-  public when(notation: string): this {
-    this.contract.when = notation;
-    return this;
-  }
-
-  public is(value: any): this {
-    this.contract.is = value;
-    return this;
-  }
-
-  public with(...args: unknown[]): Superposition {
-    this.contract.with ??= args;
-
+  public inScope(scope: HiggsField): Superposition {
     // If both upon and build are missing, we cannot create a particle
     if (!this.contract.upon || !this.contract.build) {
       throw new Error('Missing required properties');
     }
 
-    // Adds a new particle creation contract
+    this.contract.scope = scope;
+
     this.parent.addContract({
       upon: this.contract.upon,
       when: this.contract.when,
-      with: this.contract.with,
       is: this.contract.is,
       build: this.contract.build,
+      using: this.contract.using,
+      scope: this.contract.scope,
     });
 
     // Reset the particle that were being configured
@@ -49,8 +44,14 @@ export class ParticleContractBuilder {
     this.contract.when = undefined;
     this.contract.is = undefined;
     this.contract.build = undefined;
-    this.contract.with = undefined;
+    this.contract.using = undefined;
+    this.contract.scope = undefined;
 
     return this.parent;
+  }
+
+  public using(...args: unknown[]): this {
+    this.contract.using = args;
+    return this;
   }
 }
