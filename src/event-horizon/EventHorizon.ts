40eeeac3098a8bec9a48cfffed1f67285d986bf2
Alex Souza <alex.webdevelloper@gmail.com>
2025-06-24 18:51:01 -0300

    refactor: move and improve docs for event horizon and query builder

---
+++ src/event-horizon/EventHorizon.ts
@@ -0,0 +1,34 @@
+import { CausalityLog, Event } from '../shared/types/Events.types';
+import { QueryBuilder } from './builders/Query.builder';
+
+/**
+ * The limit from which data cannot escape.
+ * It holds a history of all past events and allows the search of it
+ */
+export class EventHorizon {
+  public readonly causalityLogs: CausalityLog[] = [];
+
+  /**
+   * Adds a new event record to the causality log.
+   * This method should be called whenever a new event is emitted.
+   * @param event The name of the event that occurred.
+   * @param args The arguments that were passed when the event was emitted.
+   */
+  public add<EventArgs extends unknown[] = unknown[]>(
+    event: Event,
+    ...args: EventArgs
+  ): this {
+    this.causalityLogs.push({ event, args });
+    return this;
+  }
+
+  /**
+   * Begins a new query of past events.
+   * Returns a new QueryBuilder instance, allowing for a safe, chainable,
+   * and state-isolated way to search the causality log.
+   * @returns A new instance of `QueryBuilder`.
+   */
+  public query(): QueryBuilder {
+    return new QueryBuilder(this.causalityLogs);
+  }
+}
---
+++ src/event-horizon/builders/Query.builder.ts
@@ -0,0 +1,88 @@
+import { Notation } from '../../shared/Notation';
+import { CausalityLog } from '../../shared/types/Events.types';
+
+export class QueryBuilder {
+  private fromClause?: string;
+  private usingClause?: string;
+  private argClause?: number;
+  private whereClause?: unknown;
+
+  constructor(private readonly causalityLogs: CausalityLog[]) {}
+
+  /**
+   * Filters the search to only include logs from a specific event.
+   * If not called, the query will search through all events.
+   * @param event The name of the event to filter by.
+   */
+  public from(event: string): this {
+    this.fromClause = event;
+    return this;
+  }
+
+  /**
+   * Specifies a dot-notation path to access nested data within an event's argument.
+   * This is necessary when an argument is an object or array.
+   * @param notation The dot-notation string (e.g., 'user.profile.name').
+   */
+  public using(notation?: string): this {
+    this.usingClause = notation;
+    return this;
+  }
+
+  /**
+   * Asserts that the final resolved data must be strictly equal to a specific value.
+   * If the data does not match, the query continues searching for the next match.
+   * @param value The value to assert against the resolved data.
+   */
+  public where(value: unknown): this {
+    this.whereClause = value;
+    return this;
+  }
+
+  /**
+   * The arg number to use when querying data.
+   * Eg. If an event `test` generates two arguments, like `{ user: 'John' }` and `{ pass: 123 }`,
+   * and we provide the arg number as 0, the query will be run against the first argument `{ user: 'John' }`
+   */
+  public arg(argNumber?: number): this {
+    this.argClause = argNumber;
+    return this;
+  }
+
+  /**
+   * Executes the built query and returns the first result found.
+   * The search is performed from the most recent event to the oldest.
+   * @template T The expected type of the returned data.
+   * @returns The first piece of data that matches all query clauses, or `undefined` if no match is found.
+   */
+  public get<T>(): T | undefined {
+    // Filter the past events
+    const potentialDataPoints = [...this.causalityLogs]
+      .reverse()
+      .filter((log) => !this.fromClause || log.event === this.fromClause)
+      .flatMap((log) =>
+        this.argClause !== undefined ? [log.args[this.argClause]] : log.args
+      );
+
+    const notationInstance = this.usingClause
+      ? Notation.create(this.usingClause)
+      : null;
+
+    for (const data of potentialDataPoints) {
+      if (this.usingClause && (typeof data !== 'object' || data === null)) {
+        continue;
+      }
+
+      // Parses the data using the Notation utility
+      const parsedData = notationInstance
+        ? notationInstance.getData(data)
+        : data;
+
+      if (this.whereClause === undefined || parsedData === this.whereClause) {
+        return parsedData as T;
+      }
+    }
+
+    return undefined;
+  }
+}
