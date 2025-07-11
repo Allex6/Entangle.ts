dd83a547327378d84c9a87b4a402fa96525bce4a
Alex Souza <alex.webdevelloper@gmail.com>
2025-07-11 19:44:16 -0300

    feat: :tada: add initial export structure for core modules in index.ts

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+export { Aether, InMemoryAether } from './aether/index';
+export { Superposition } from './superposition/Superposition';
+export { HiggsField } from './higgs-field/HiggsField';
+export { EventHorizon } from './event-horizon/EventHorizon';
+export { QueryBuilder } from './event-horizon/builders/Query.builder';
+export { HawkingRadiation } from './hawking-radiation/HawkingRadiation';
+export { QuantumPointer } from './quantum-pointer/QuantumPointer';
+export { Notation } from './shared/Notation';
+export { Event, CausalityLog } from './shared/types/Events.types';
+export { Interaction, Target } from './shared/types/Interactions.types';
+export {
+  Particle,
+  ParticleProperties,
+  ParticleLifecycle,
+  Entanglement,
+  Boson,
+} from './shared/types/Particles.types';
+export {
+  Callable,
+  Callback,
+  Promisable,
+  Resolvable,
+  ResolvableArgs,
+  MethodKeys,
+} from './shared/types/Utils.types';
